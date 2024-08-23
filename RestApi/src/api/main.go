2c2393ad11294d6ff65d9128b4dbf0ed5cd93002
Paras Kumar <paraskumarvhp5@gmail.com>
2024-08-23 16:43:47 +0530

    My First Commit

--- RestApi/src/api/main.go
+++ RestApi/src/api/main.go
@@ -10,5 +10,7 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/greeting", router.Greeting).Methods("GET")
+	r.HandleFunc("/Student", router.StuDetails).Methods("POST")
+	r.HandleFunc("/Arithmetic", router.SumOfDigit).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- RestApi/src/api/router/controller.go
+++ RestApi/src/api/router/controller.go
@@ -1,9 +1,42 @@
 package router
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type student struct {
+	Name  string `json:"name"`
+	Roll  int    `json:"roll"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
+func StuDetails(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	st := student{}
+	fmt.Println(st, "Before calling a API")
+	json.NewDecoder(r.Body).Decode(&st)
+	fmt.Println(st, "After calling a API")
+}
 
 func Greeting(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello World"))
+}
+
+func SumOfDigit(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	type arithmetic struct {
+		A int `json:"a"`
+		B int `json:"b"`
+	}
+	at := arithmetic{}
+	json.NewDecoder(r.Body).Decode(&at)
+	C := at.A + at.B
+	s := "Sum Of " + strconv.Itoa(at.A) + " And " + strconv.Itoa(at.B) + " = " + strconv.Itoa(C)
+	json.NewEncoder(w).Encode(&s)
 }
